90153be8a79c1747a3afbe7f05e5edb92d30bb2f
duckba1994 <77369222+duckba1994@users.noreply.github.com>
2022-07-25 17:06:36 +0700

    update

---
+++ app/src/main/java/com/example/myapplication/ui/scanin/PopupDetailFragment.java
@@ -0,0 +1,68 @@
+package com.example.myapplication.ui.scanin;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.myapplication.R;
+
+public class PopupDetailFragment extends DialogFragment implements View.OnClickListener {
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_DeviceDefault_Light_NoActionBar_Fullscreen);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog d = getDialog();
+        if (d != null) {
+            int width = ViewGroup.LayoutParams.WRAP_CONTENT;
+            int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+            d.getWindow().setGravity(Gravity.CENTER | Gravity.BOTTOM);
+
+            d.getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.popup_window, container, false);
+
+        // get a variable from MainActivity.java when calling this Dialog. The variable is in the form of key and value.
+        Bundle mArgs = getArguments();
+
+        String msg = mArgs.getString("key");
+        String thisTitle = mArgs.getString("title");
+
+
+        final TextView header = (TextView) root.findViewById(R.id.header_text);
+        header.setText(thisTitle);
+
+//        final TextView myMsg =  (TextView) root.findViewById(R.id.expText);
+//        myMsg.setText(msg);
+//        myMsg.setTextSize(20);
+
+        Button btnOK = (Button) root.findViewById(R.id.btnExpOK);
+        btnOK.setOnClickListener(PopupDetailFragment.this);
+        return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        dismiss();
+
+    }
+}
--- app/src/main/java/com/example/myapplication/ui/scanin/ScanInFragment.java
+++ app/src/main/java/com/example/myapplication/ui/scanin/ScanInFragment.java
@@ -1,38 +1,35 @@
 package com.example.myapplication.ui.scanin;
 
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
-
-import static androidx.core.content.ContextCompat.getSystemService;
-
-import android.annotation.SuppressLint;
-import android.app.Dialog;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentScanInBinding;
 
-public class ScanInFragment extends Fragment implements View.OnClickListener {
+public class ScanInFragment extends Fragment implements View.OnClickListener, View.OnKeyListener {
 
     private FragmentScanInBinding binding;
     private View view;
-    private  View view_popup;
+    private View view_popup;
+    private String str_rfid;
+
+    Button btn,btn_search_doc,btn_search_product;
+    EditText txt_rfid, txt_bc_ref1, txt_bc_ref2, txt_product_ref1, txt_product_ref2, txt_from_ref1, txt_from_ref2, txt_rack, txt_loc, txt_doc_no, txt_product_name, txt_qty_total_new;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,8 +46,31 @@ public class ScanInFragment extends Fragment implements View.OnClickListener {
     }
 
     private void init() {
-        Button btn = (Button) view.findViewById(R.id.btn_search_rfid);
+        //SET EDITTEXT
+        txt_rfid = (EditText) view.findViewById(R.id.txt_rfid);
+        txt_product_ref1 = (EditText) view.findViewById(R.id.txt_product_ref1);
+        txt_product_ref2 = (EditText) view.findViewById(R.id.txt_product_ref2);
+        txt_from_ref1 = (EditText) view.findViewById(R.id.txt_from_ref1);
+        txt_from_ref2 = (EditText) view.findViewById(R.id.txt_from_ref2);
+        txt_rack = (EditText) view.findViewById(R.id.txt_rack);
+        txt_loc = (EditText) view.findViewById(R.id.txt_loc);
+        txt_doc_no = (EditText) view.findViewById(R.id.txt_doc_no);
+        txt_bc_ref1 = (EditText) view.findViewById(R.id.txt_bc_ref1);
+        txt_bc_ref2 = (EditText) view.findViewById(R.id.txt_bc_ref2);
+        txt_product_name = (EditText) view.findViewById(R.id.txt_bc_ref1);
+
+        //SET BUTTON
+        btn = (Button) view.findViewById(R.id.btn_search_rfid);
+        btn_search_doc = (Button) view.findViewById(R.id.btn_search_doc);
+        btn_search_product = (Button) view.findViewById(R.id.btn_search_product);
+
+        txt_rfid.setOnKeyListener(this);
         btn.setOnClickListener(this);
+        btn_search_doc.setOnClickListener(this);
+        btn_search_product.setOnClickListener(this);
+
+//        txt_product_name.setFocusableInTouchMode(true);
+//        txt_product_name.setFocusable(true);
     }
 
     @Override
@@ -66,20 +86,104 @@ public class ScanInFragment extends Fragment implements View.OnClickListener {
         switch (id) {
 
             case R.id.btn_search_rfid:
-
-//                DialogFragment dialog
-//                        = new DialogFragment();
-//                dialog.show(getFragmentManager(),
-//                        "MyCustomDialog");
-
-//                final Dialog fbDialogue = new Dialog(this.getContext(), android.R.style.Theme_Black_NoTitleBar);
-//
-//                fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
-//                fbDialogue.setContentView(R.layout.popup_window);
-//
-//                fbDialogue.setCancelable(true);
-//                fbDialogue.show();
+                showDialog(this.getView());
                 break;
+            case  R.id.btn_search_doc:
+                showDialog(this.getView());
+                break;
+            case R.id.btn_search_product:
+                showDialog_DetailProduct(this.getView());
+
+                break;
+
         }
+    }
+    public void showDialog(View view){
+
+        String myTitle = "Detail Head IN";
+        String myMessage = "\r\n1. สวัสดีครับ " +
+                "\r\n2. jadf djasf dsjfsd fjddfjkdsf " +
+                "\r\n3. fkjd fdsklfj dfljfl ladks" +
+                "\r\n4. jdfjds fds";
+
+        Bundle args = new Bundle();
+        args.putString("key", myMessage);
+        args.putString("title", myTitle);
+        PopupDetailFragment newFragment = new PopupDetailFragment();
+        FragmentTransaction ft = getFragmentManager ().beginTransaction();
+        newFragment.setArguments(args);
+//        newFragment.show(ft, "TAG");
+    newFragment.setShowsDialog(true);
+    }
+
+    public void showDialog_DetailProduct(View view){
+
+        String myTitle = "Detail Product IN";
+        String myMessage = "\r\n1. สวัสดีครับ " +
+                "\r\n2. jadf djasf dsjfsd fjddfjkdsf " +
+                "\r\n3. fkjd fdsklfj dfljfl ladks" +
+                "\r\n4. jdfjds fds";
+
+        Bundle args = new Bundle();
+        args.putString("key", myMessage);
+        args.putString("title", myTitle);
+        PopUpDatailProductFragment newFragment = new PopUpDatailProductFragment();
+        FragmentTransaction ft = getFragmentManager ().beginTransaction();
+        newFragment.setArguments(args);
+        newFragment.show(ft, "TAG");
+    }
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+        if (i == EditorInfo.IME_ACTION_SEARCH ||
+                i == EditorInfo.IME_ACTION_DONE ||
+                keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
+                        keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+
+
+            if (!keyEvent.isShiftPressed()) {
+                switch (view.getId()) {
+                    case R.id.txt_rfid:
+                        check_txt_rfid();
+                        break;
+
+                }
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    private void check_txt_rfid() {
+        str_rfid = txt_rfid.toString();
+        if (txt_rfid.length() <= 0) {
+            msg("ระบุเลข RFID");
+            txt_rfid.requestFocus();
+        } else {
+            if(check_rfid()){
+                txt_rfid.setEnabled(false);
+                txt_product_name.requestFocus();
+            }else{
+                msg("ไม่พบเลข RFID ที่ระบุ");
+            }
+        }
+    }
+    private boolean check_rfid(){
+        return  false;
+    }
+
+    private  void msg(String msg){
+        Toast toast = Toast.makeText(this.getContext(), msg, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toast.cancel();
+            }
+        }, 59000);
     }
 }
